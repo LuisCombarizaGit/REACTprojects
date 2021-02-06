f6778e87d0d66c3c985466df495857ee7a4fb726
Luis Combariza <luis_combariza@outlook.com>
2021-02-05 20:04:18 -0700

    Updated retrived data to be destructured so as to only use the data needed in the application

--- corono-virus-web-app/src/api/index.js
+++ corono-virus-web-app/src/api/index.js
@@ -6,8 +6,11 @@ const url = "https://covid19.mathdro.id/api";
 // Asynchrounous function that returns the call fro the API
 export const fetchData = async () => {
   try {
-    const response = await axios.get(url);
+    // Destructure data to only use the data needed in the application
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
 
-    return response;
+    return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {}
 };
