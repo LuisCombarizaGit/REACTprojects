f130f131f6cfcb83a12602d8cc922e0adfb8dd97
Luis Combariza <luis_combariza@outlook.com>
2021-02-05 19:38:36 -0700

    Fixed some component issues: not compiling due to wrong DOM element

---
+++ corono-virus-web-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Cards, Charts, CountryPicker } from "./components";
+import styles from "./App.module.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Charts />
+      </div>
+    );
+  }
+}
+
+export default App;
