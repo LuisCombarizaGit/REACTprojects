fdf1871bdf2b14f7439e1b983dcb650368bbe8ff
Luis Combariza <luis_combariza@outlook.com>
2021-02-08 15:24:21 -0700

    Updated Cards.jsx to show live data of confirmed cases

--- corono-virus-web-app/src/components/Cards/Cards.jsx
+++ corono-virus-web-app/src/components/Cards/Cards.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import styles from "./Cards.module.css";
 
-function Cards(props) {
-  console.log(props);
-
+function Cards({ data: { confirmed, recovered, deaths, lastUpdate } }) {
+  if (!confirmed) {
+    return "Loading...";
+  }
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
@@ -13,10 +14,34 @@ function Cards(props) {
             <Typography color="textSecondary" gutterBottom>
               Infected
             </Typography>
-            <Typography variant="h5">REAL DATA</Typography>
+            <Typography variant="h5">{confirmed.value}</Typography>
             <Typography color="textSecondary">REAL DATE</Typography>
             <Typography variant="body2">
               Number of active cases of COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recovered
+            </Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">
+              Number of recoveries from COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">
+              Number of deaths caused by COVID-19
             </Typography>
           </CardContent>
         </Grid>
