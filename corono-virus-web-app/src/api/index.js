0d87fb25190b1d1fbee799578025d97560fa1e9b
Luis Combariza <luis_combariza@outlook.com>
2021-02-08 16:03:11 -0700

    Updated Charts.js and downloaded chart.js 2.9.4

--- corono-virus-web-app/src/api/index.js
+++ corono-virus-web-app/src/api/index.js
@@ -6,12 +6,19 @@ const url = "https://covid19.mathdro.id/api";
 // Asynchrounous function that returns the call fro the API
 export const fetchData = async () => {
   try {
-      
     // Destructure data to only use the data needed in the application
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
     } = await axios.get(url);
 
     return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {}
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    console.log(data);
   } catch (error) {}
 };
--- corono-virus-web-app/src/components/Charts/Charts.jsx
+++ corono-virus-web-app/src/components/Charts/Charts.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Chart.module.css";
 
 function Charts() {
+  const [dailyData, setDailyData] = useState({});
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+
+    fetchAPI();
+  });
+
+  const lineChart = dailyData[0] ? (
+    <Line
+      data={{
+        labels: "",
+        datasets: [{}, {}],
+      }}
+    />
+  ) : null;
+
   return (
     <div>
       <h1>Charts</h1>
