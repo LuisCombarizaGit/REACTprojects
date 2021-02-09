bba36deb5ba51e78200b107326561b2be5aeb7ca
Luis Combariza <luis_combariza@outlook.com>
2021-02-08 19:12:21 -0700

    Updated App.js to receive API call , convert it to Json and parse country data

--- covid-tracker-web-application/src/App.js
+++ covid-tracker-web-application/src/App.js
@@ -1,7 +1,41 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 
 function App() {
-  return <div className="App">Hello</div>;
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  });
+
+  return (
+    <div className="App">
+      <div className="app_header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+    </div>
+  );
 }
 
 export default App;
