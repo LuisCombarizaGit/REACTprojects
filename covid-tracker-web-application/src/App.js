21589ff3c369bf1c47a7d15d8c20f887298bd36a
Luis Combariza <luis_combariza@outlook.com>
2021-02-08 19:39:11 -0700

    Updated Js and CSS for information cards

--- covid-tracker-web-application/src/App.js
+++ covid-tracker-web-application/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { MenuItem, FormControl, Select } from "@material-ui/core";
+import InfoBox from "./InfoBox";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -21,18 +23,32 @@ function App() {
     getCountriesData();
   });
 
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+
+    console.log(">>>>>>", countryCode);
+
+    setCountry(countryCode);
+  };
+
   return (
     <div className="App">
       <div className="app_header">
         <h1>COVID-19 TRACKER</h1>
         <FormControl className="app_dropdown">
-          <Select variant="outlined" value="abc">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
             <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
           </Select>
         </FormControl>
+      </div>
+
+      <div className="app_stats">
+        <InfoBox title="Coronovirus Cases" cases={1234} total={3000} />
+        <InfoBox title="Recovered" cases={1234} total={123123} />
+        <InfoBox title="Deaths" cases={1234} total={123123} />
       </div>
     </div>
   );
