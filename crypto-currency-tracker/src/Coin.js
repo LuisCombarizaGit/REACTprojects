00a679d65c19efbce237a710661d8f3ee19e3124
Luis Combariza <luis_combariza@outlook.com>
2021-02-10 20:13:39 -0700

    Added css styling file

--- crypto-currency-tracker/src/Coin.js
+++ crypto-currency-tracker/src/Coin.js
@@ -1,6 +1,7 @@
 import React from "react";
+import "./Coin.css";
 
-const Coin = ({ name, img, symbol, price, volume }) => {
+const Coin = ({ name, img, symbol, price, volume, priceChange, marketcap }) => {
   return (
     <div className="coin-container">
       <div className="coin-row">
@@ -13,10 +14,13 @@ const Coin = ({ name, img, symbol, price, volume }) => {
           <p className="coin-price">${price}</p>
           <p className="coin-volume">${volume.toLocaleString()}</p>
           {priceChange < 0 ? (
-            <p className="coin-percent red"> {priceChange.toFixed(2)}</p>
+            <p className="coin-percent red"> {priceChange.toFixed(2)}%</p>
           ) : (
             <p className="coin-percent green"> {priceChange.toFixed(2)}%</p>
           )}
+          <p className="coin-marketcap">
+            Mkt Cap: ${marketcap.toLocaleString()}{" "}
+          </p>
         </div>
       </div>
     </div>
