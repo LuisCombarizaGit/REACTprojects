dc929d17472e4d39ad6ce2829b1a08258917d81b
Luis Combariza <luis_combariza@outlook.com>
2021-02-12 16:55:34 -0700

    Added initial files

---
+++ whatsapp-mern-project/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
---
+++ whatsapp-mern-project/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxuve-gXiE79mCV5LXNRE7RiMlaWkbdl0",
+  authDomain: "whatsapp-web-application.firebaseapp.com",
+  projectId: "whatsapp-web-application",
+  storageBucket: "whatsapp-web-application.appspot.com",
+  messagingSenderId: "612079953595",
+  appId: "1:612079953595:web:5d7bf987027b29a8adab9e",
+};
