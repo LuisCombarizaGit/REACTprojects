e1e61e4430926cea4ba89b349225c2ad4fe05e05
Luis Combariza <luis_combariza@outlook.com>
2021-02-12 18:58:04 -0700

    Added and updated css files for side bar containers

--- whatsapp-mern-project/src/Sidebar.js
+++ whatsapp-mern-project/src/Sidebar.js
@@ -1,8 +1,46 @@
 import React from "react";
 import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import { Avatar, IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
+import SidebarChat from "./sidebarChat";
 
 function Sidebar() {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar_header">
+        <Avatar
+          src="https://t4.ftcdn.net/jpg/02/64/72/99/360_F_264729912_3yJ8DMpnc0EdpI9k18nZnVh82yiopLWj.jpg"
+          alt="Robot face picture"
+        />
+        <div className="sidebar_headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar_search">
+        <div className="sidebar_searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+
+      <div className="sidebar_chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
