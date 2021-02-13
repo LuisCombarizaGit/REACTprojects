dd532df8492f586764c6ef4c4800fc2d6d4f2b71
Luis Combariza <luis_combariza@outlook.com>
2021-02-12 17:37:26 -0700

    Added and updated js and css files

--- whatsapp-mern-project/src/App.js
+++ whatsapp-mern-project/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+import "./App.css";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar />
-      <Chat />
+    <div className="app">
+      <div className="app_body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }
