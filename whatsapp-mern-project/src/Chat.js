c977db153c4fc2fbd1ea6630fb69a644df167e0d
Luis Combariza <luis_combariza@outlook.com>
2021-02-12 19:17:16 -0700

    Updated css files

--- whatsapp-mern-project/src/Chat.js
+++ whatsapp-mern-project/src/Chat.js
@@ -1,8 +1,32 @@
 import React from "react";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import { Avatar, IconButton } from "@material-ui/core";
 import "./Chat.css";
 
 function Chat() {
-  return <div>Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chat_header">
+        <Avatar />
+        <div className="chat_headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at ...</p>
+        </div>
+
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
