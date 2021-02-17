9cff14ef49c8a0ea1bbfba344a43c54c07d34315
Luis Combariza <luis_combariza@outlook.com>
2021-02-16 19:16:00 -0700

    Fixed database communication bug

--- whatsapp-mern-backend/server.js
+++ whatsapp-mern-backend/server.js
@@ -2,10 +2,32 @@
 import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbMessages.js";
+import Pusher from "pusher";
 
 // app config
 const app = express();
 const port = process.env.PORT || 9000;
+
+const pusher = new Pusher({
+  appId: "1157289",
+  key: "b255949f3af928cd5580",
+  secret: "227a363c09360ba8f606",
+  cluster: "us3",
+  useTLS: true,
+});
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("DB Connected");
+
+  const msgCollection = db.collection("messagecontens");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log("A Change occured:", change);
+  });
+});
 
 // middleware
 app.use(express.json());
@@ -23,6 +45,16 @@ mongoose.connect(connection_url, {
 
 // api routes
 app.get("/", (req, res) => res.status(200).send("Hello World"));
+
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
 app.post("/messages/new", (req, res) => {
   const dbMessage = req.body;
